72a434e1a4bd20cb38b3ae770fa9dbaa2e266eb7
Jean Gonçalves <jean.gnc@gmail.com>
2023-03-04 11:17:39 -0300

    Renames property

--- lib/extractor.rb
+++ lib/extractor.rb
@@ -1,9 +1,9 @@
 
 class Extractor
   DATE_REGEXP = /[0-9]{2} [a-zA-Z]{3}/
-  NAME_REGEXP = /.+?(?=-|R\$)/
+  DESCRIPTION_REGEXP = /.+?(?=-|R\$)/
   VALUE_REGEXP = /(- )?R\$ ([0-9]+\.?)+\,[0-9]{2}/
-  STATEMENT_REGEXP = /((?<date>#{DATE_REGEXP})\W+(?<name>#{NAME_REGEXP})(?<value>#{VALUE_REGEXP}))/
+  STATEMENT_REGEXP = /((?<date>#{DATE_REGEXP})\W+(?<name>#{DESCRIPTION_REGEXP})(?<value>#{VALUE_REGEXP}))/
 
   def self.extract_statements(filepath)
     reader = PDF::Reader.new(filepath)
@@ -19,7 +19,7 @@ class Extractor
         line.scan(STATEMENT_REGEXP).each do |statement|
           memo << {
             date: statement[0].strip,
-            name: statement[1].strip,
+            description: statement[1].strip,
             value: statement[2].strip
           }
         end
