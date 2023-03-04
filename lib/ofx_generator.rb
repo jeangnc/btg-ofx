6dbc7be6f754f86bdbde5a1feca62892e6b0dcec
Jean Gonçalves <jean.gnc@gmail.com>
2023-03-04 11:56:22 -0300

    Generates OFX

---
+++ lib/ofx_generator.rb
@@ -0,0 +1,46 @@
+class OfxGenerator
+  def initialize(statements)
+    @statements = statements
+  end
+
+  def generate
+    <<~OFX
+    OFXHEADER:100
+    DATA:OFXSGML
+    VERSION:102
+    SECURITY:NONE
+    ENCODING:USASCII
+    CHARSET:1252
+    COMPRESSION:NONE
+    OLDFILEUID:NONE
+    NEWFILEUID:NONE
+
+    <OFX>
+      <BANKMSGSRSV1>
+        <STMTTRNRS>
+          <STMTRS>
+            <BANKTRANLIST>
+              #{statements_ofx}
+            </BANKTRANLIST>
+          </STMTRS>
+        </STMTTRNRS>
+      </BANKMSGSRSV1>
+    </OFX>
+    OFX
+  end
+
+  private
+
+  def statements_ofx
+    @statements.map do |statement|
+      <<-OFX
+      <STMTTRN>
+        <TRNTYPE>OTHER</TRNTYPE>
+        <DTPOSTED>#{statement.date.strftime('%Y%m%d')}000000[-3:GMT]</DTPOSTED>
+        <TRNAMT>#{statement.value}</TRNAMT>
+        <MEMO>#{statement.description}</MEMO>
+      </STMTTRN>
+      OFX
+    end.join("\n")
+  end
+end
