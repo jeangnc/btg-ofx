c71c7eb165a5445841c25e7c4a4b91f829c69837
Jean Gonçalves <jean.gnc@gmail.com>
2023-03-04 11:39:59 -0300

    Implements data transformation

---
+++ lib/data_transformer.rb
@@ -0,0 +1,29 @@
+require 'date'
+
+class DataTransformer
+  MONTHS = {
+    'Jan' => 1,
+    'Fev' => 2,
+    'Mar' => 3,
+    'Abr' => 4,
+    'Mai' => 5,
+    'Jun' => 6,
+    'Jul' => 7,
+    'Ago' => 8,
+    'Set' => 9,
+    'Out' => 10,
+    'Nov' => 11,
+    'Dez' => 12
+  }
+
+  def self.value(value_string)
+    value_string.gsub(' R$ ', '').gsub('.', '').gsub(',', '.').to_f
+  end
+
+  def self.date(date_string)
+    day, month = date_string.split(' ')
+    month = MONTHS[month]
+
+    Date.parse "#{Date.today.year}-#{month}-#{day}"
+  end
+end
